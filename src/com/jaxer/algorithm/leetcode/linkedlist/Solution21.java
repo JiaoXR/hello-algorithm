2b8e9c994eecf42fb27c562f4ad51056c1274033
jiaoxiangru <jiaoxiangru@gmail.com>
2021-09-24 16:36:20 +0800

    leetcode86: partition list

--- src/com/jaxer/algorithm/leetcode/linkedlist/Solution21.java
+++ src/com/jaxer/algorithm/leetcode/linkedlist/Solution21.java
@@ -7,7 +7,26 @@ package com.jaxer.algorithm.leetcode.linkedlist;
  * @since 2021/9/16 22:08
  */
 public class Solution21 {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    /**
+     * 递归
+     */
+    public ListNode mergeTwoLists02(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists02(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists02(l1, l2.next);
+            return l2;
+        }
+    }
+
+    /**
+     * 迭代
+     */
+    public ListNode mergeTwoLists01(ListNode l1, ListNode l2) {
         ListNode newHead = new ListNode();
         ListNode curr = newHead;
         while (l1 != null && l2 != null) {
---
+++ src/com/jaxer/algorithm/leetcode/linkedlist/Solution86.java
@@ -0,0 +1,53 @@
+package com.jaxer.algorithm.leetcode.linkedlist;
+
+/**
+ * 分隔链表：https://leetcode-cn.com/problems/partition-list/
+ *
+ * @author jiaoxiangru
+ * @since 2021/9/24 15:53
+ */
+public class Solution86 {
+    /**
+     * 分隔链表
+     *
+     * @param head 给定链表头
+     * @param x    特定值
+     * @return 分隔后的链表
+     */
+    public ListNode partition(ListNode head, int x) {
+        // 准备两个链表：一个为小于特定值，一个大于等于特定值
+        ListNode sH = null;
+        ListNode sT = null;
+        ListNode bH = null;
+        ListNode bT = null;
+        while (head != null) {
+            // 注意循环问题
+            ListNode next = head.next;
+            head.next = null;
+            // 小于特定值的元素放到链表一
+            if (head.val < x) {
+                if (sH == null) {
+                    sH = head;
+                } else {
+                    sT.next = head;
+                }
+                sT = head;
+            } else {
+                // 大于等于特定值的元素放到链表二
+                if (bH == null) {
+                    bH = head;
+                } else {
+                    bT.next = head;
+                }
+                bT = head;
+            }
+            head = next;
+        }
+        // 注意判空
+        if (sT != null) {
+            sT.next = bH;
+            return sH;
+        }
+        return bH;
+    }
+}
